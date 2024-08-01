45db0cac5c2b7c4ed79756266c3a6a7027d0e5d5
YaafarDiaz <yaafar.diaz@gmail.com>
2024-07-31 21:48:45 -0500

    Add Screenshots

--- repossh/Settings/settings.py
+++ repossh/Settings/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent
 SECRET_KEY = 'django-insecure-5jnqks+r6t@+%$#e%@$y$a^em@b3u*4%mcwy62!m)vu+1zu^!^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
